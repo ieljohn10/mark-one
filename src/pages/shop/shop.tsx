08e9b3e8133e2a80b6f4984906563df3f118e058
Yeyel <Yeyel@wf.local>
2023-03-28 18:44:30 +0800

    day 3 added api

--- src/pages/shop/shop.tsx
+++ src/pages/shop/shop.tsx
@@ -1,10 +1,36 @@
-import style from './shop.module.scss';
+import { useEffect, useState } from 'react';
+import './shop.scss';
 
 const ShopPage = () => {
-    return (
-        <div className={style.aboutPage}>
-            Shop Page
-        </div>
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(()=>{
+        // https://www.fakeshop-api.com/
+        (() => {
+            fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>setData(json))
+           setLoading(false)
+        })()
+       
+    },[])
+    console.log(data)
+
+    return (        
+        <div className='shopPage'>
+					<div className="productContainer">
+						{data.map((item:any)=>{
+							return(
+									<div className="productItem" key={item.id}>
+										<img className='card-image' src={item.image} alt=''/>
+										<div className="productitle">{item.title}</div>
+										{/* <div className="heading">{item.description}</div> */}
+									</div>
+								)
+						})}
+					</div>
+        </div> 
     )
 }
 
