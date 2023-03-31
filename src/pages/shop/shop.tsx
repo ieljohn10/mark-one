44cebe04dcdd067d671320e57aadaa666d6b57f5
yeyel <arielglvz@gmail.com>
2023-03-31 17:56:11 +0800

    day 6

--- src/pages/shop/shop.tsx
+++ src/pages/shop/shop.tsx
@@ -1,11 +1,10 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './shop.scss';
 
 const ShopPage = () => {
-    const [data, setData] = useState([])
-    // const [loading, setLoading] = useState(true)
-		const [searchVal, setSearchVal] = useState('');
+    const [data, setData] = useState([]);
+
 
 		useEffect(() => {
 			axios.get('https://fakestoreapi.com/products')
@@ -13,37 +12,10 @@ const ShopPage = () => {
 			.catch(error => console.log(error));
 		}, [])
 
-		const handleInput = (e:any) => {
-			setSearchVal(e.target.value);
-			// console.log(e.target.value)
-		}
-
-		const handleFilter = () => {
-			console.log(searchVal)
-			const dataFiltered = data.filter((item:any) => {
-				// return item.includes(searchVal);
-			});
-			console.log(dataFiltered)
-		}
-
-
-
     return (        
-        <div className='shopPage'>
-					<input 
-						onChange={handleInput}
-						placeholder="Search Products"
-					/>
-					<button onClick={handleFilter}>Search</button>
-					<div className="productContainer">
-					{/* <ul>
-          {data.map((product:any) => {
-            return <li key={product} className='list-item'><a href='#'>{product}</a></li>
-          })}
-        </ul> */}
-					</div>
+        <div className='shopPage'>			
+
         </div> 
     )
-}
-
+	}
 export default ShopPage;
