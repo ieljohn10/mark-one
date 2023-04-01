9cbd8f149346e5d8e9cd98fbb924d760daefe989
yeyel <arielglvz@gmail.com>
2023-04-01 17:15:43 +0800

    day 6

--- src/pages/shop/shop.tsx
+++ src/pages/shop/shop.tsx
@@ -1,21 +1,36 @@
 import axios from 'axios';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './shop.scss';
 
 const ShopPage = () => {
-    const [data, setData] = useState([]);
+	const [data, setData] = useState([]);
 
+	useEffect(() => {
+		axios.get('https://fakestoreapi.com/products')
+		.then(response => setData(response.data))
+		.catch(error => console.log(error));
+		
+	},[])
+	console.log(data);
 
-		useEffect(() => {
-			axios.get('https://fakestoreapi.com/products')
-			.then(response => setData(response.data))
-			.catch(error => console.log(error));
-		}, [])
-
-    return (        
-        <div className='shopPage'>			
-
-        </div> 
-    )
-	}
+	return (        
+		<div className='shopPage'>			
+			<div className="card-container">
+				{data.map((item:any) => {
+					return(
+						<div className="card-item" key={item.id}>
+							<div className="image-container">
+								<img src={item.image} alt={item.title} />
+							</div>
+							<div className="card-desc-container">
+								<h4 className="title">{item.title}</h4>
+								{/* <p className="desc">{item.description}</p> */}
+							</div>
+						</div>
+					)
+				})}
+			</div>
+		</div> 
+	)
+}
 export default ShopPage;
